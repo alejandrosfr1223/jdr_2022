8cbcd358c9f0ce8df528f71f91b5929a772038e4
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2022-09-19 01:23:13 -0400

    cambios varios al home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,7 @@
                     </div>
                     <div class="rightdivide">
                         <p id="infomaintext">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Capitán Juan del Rincón Aguilar, Conquistador de las Sierras Nevadas del Nuevo Reino de Granada (S. XVI).
                         </p>
                     </div>
                 </div>
@@ -43,26 +43,61 @@
 
         <div class="whitebg" style="position: relative;">
             <div id="containermv">
-                <div class="mv_cont textinfofisidoro">
+                <div class="cont_welc_jdr textinfofisidoro">
+                    <center>
+                        <h1 class="first_key" style="color: #cca766;">Contexto histórico</h1>
+                    </center>
                     <p class="textisidoro">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        La situación en los reinos de España a partir de 1492 va a cambiar por dos aspectos fundamentales, la expulsión de los judíos y el descubrimiento del Nuevo Mundo. Muchos de los que optaron por la conversión van a ver que continúan siendo perseguidos, tanto por la Inquisición como por los cristianos viejos. Ello los mueve a embarcarse en grandes empresas, porque no tienen nada que perder.
                     </p>
                 </div>
                 <div class="mv_cont">
-                    <img src="{{ asset('/img/bgdivinapastora1.png') }}" >
+                    <img src="{{ asset('/img/tree.png') }}" >
                 </div>
             </div>
         </div>
 
         <div class="bluebg" style="position: relative;">
-            <div id="containermv2">
-                <div id="photo_bg_dp2">
+            <div id="containermv2" class="textcontainers">
+                <div id="photo_bg_dp2" style="margin: auto;">
                     <img class="changecolorpngsyellow" src="{{ asset('/img/logos/jdr.png') }}" >
                 </div>
                 <div class="textinfofisidoro tex">
+                    <center>
+                        <h1 class="first_key" style="color: #cca766;">El territorio</h1>
+                    </center>
                     <p class="textisidoro textpresente">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        La vida de Juan del Rincón se desarrolla principalmente en un área por explorar entre los territorios pertenecientes a la Audiencia de Santa Fe y a los del Nuevo Reino de Granada. La fundación de Pamplona será el punto de partida de diferentes expediciones, con el fin de pacificar a los indios y fundar nuevas ciudades.
                     </p>
+                </div>
+            </div>
+        </div>
+
+        <div style="position: relative;">
+            <div class="changedirection textcontainers">
+                <div class="textinfofisidoro tex">
+                    <center>
+                        <h1 class="first_key" style="color: #12313a;">La familia</h1>
+                    </center>
+                    <p class="textisidoro textpresente" style="color: #12313a;">
+                        Analizaremos los diferentes lazos familiares que se van urdiendo a través del tiempo en sus dos vertientes, la ascendente y la descendente. De esta manera podremos avanzar en la búsqueda y el descubrimiento del antepasado sefardí de Juan del Rincón y poner en comunicación a las distintas ramas de sus descendientes en territorio americano.
+                    </p>
+                </div>
+                <div id="photo_bg_dp2" style="margin: auto;">
+                    <img class="changecolorpngsyellow" src="{{ asset('/img/logos/jdr.png') }}" >
+                </div>
+            </div>
+        </div>
+
+        <div class="bluebg" style="position: relative;">
+            <div id="containermv2" class="textcontainers">
+                <div class="textinfofisidoro tex">
+                    <center>
+                        <h1 class="first_key" style="color: #cca766;">Fuentes documentales y bibliográficas</h1>
+                        <p class="textisidoro textpresente">
+                            Las fuentes son las que nos ayudan a comprender los hechos históricos. Sin los documentos y los libros que interpretan los datos obtenidos de ellos, no hay Historia. Pretendemos compilar aquí todos los libros y documentos que hagan referencia a Juan del Rincón.
+                        </p>
+                    </center>
                 </div>
             </div>
         </div>
@@ -71,13 +106,13 @@
             <img src="{{ asset('/img/home/logo-overlay2.png') }}" class="backg-ovrl3 bigscreen ol2-l">
             <img src="{{ asset('/img/logos/vector-logo.svg') }}" class="backg-ovrl3 smallscreen">
             <div id="div_past_pestanas2">
-                <a href="{{ route('materiales.home') }}" class="dp_cont">
+                <a href="{{ route('materiales.home') }}" style="margin: auto;" class="dp_cont">
                     <h2>Materiales Compilados</h2>
                 </a>
-                <a href="{{ route('agenealogico.home') }}" class="dp_cont">
+                <a href="{{ route('agenealogico.home') }}" style="margin: auto;" class="dp_cont">
                     <h2>Árbol Genealógico</h2>
                 </a>
-                <a href="{{ route('descendencia.home') }}" class="dp_cont">
+                <a href="{{ route('descendencia.home') }}" style="margin: auto;" class="dp_cont">
                     <h2>Descendencia</h2>
                 </a>
             </div>
@@ -90,7 +125,7 @@
                     
                     <div class="leftdivide" id="dep_info_cnt">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Te invitamos a colaborar con esta investigación histórico-genealógica en torno al capitán Juan del Rincón, descubridor y poblador de las Sierras Nevadas del Nuevo Reino de Granada, cuyo objeto es demostrar, mediante documentación fidedigna, el origen sefardí de este tronco común de muchos apellidos hispanoamericanos.
                         </p>
                     </div>
                     <div class="rightdivide row" id="btns_cnt_dept">
